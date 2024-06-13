4627929a63c8ef78986844f5da508ab85872bb8d
woolflare <167610549+woolflare@users.noreply.github.com>
2024-06-14 02:23:40 +0800

    Update generate-html.js

--- generate-html.js
+++ generate-html.js
@@ -11,50 +11,54 @@ const generateIndexContent = (packages) => {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Automatic installation script collection.">
+    <meta name="description" content="One-Click Script Installation Assembly.">
     <title>EasyInstall - easyinstall.net</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/1.0.1/css/bulma.min.css">
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="/">EasyInstall</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#usage">Usage</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#packages">Packages</a>
-                    </li>
-                </ul>
+    <div class="hero is-fullheight">
+        <div class="hero-head">
+            <header>
+                <nav class="navbar has-shadow">
+                    <div class="container">
+                        <div class="navbar-brand">
+                            <a class="navbar-item" href="/">
+                                <span class="title">📦 EasyInstall</span>
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+        </div>
+        <div class="hero-body">
+            <div class="container">
+                <div class="columns">
+                    <div class="column is-8 is-offset-2">
+                        <div class="content">
+                            <h1>Usage</h1>
+                            <p>To install a specific package using <code>EasyInstall</code>, use the following <code>curl</code> command:</p>
+                            <pre>curl -s https://ezi.sh/[package_name] | sh</pre>
+                            <p>Replace <code>[package_name]</code> with the name of the desired package. For example, to install <code>homebrew</code>, you would use:</p>
+                            <pre>curl -s https://ezi.sh/homebrew | sh</pre>
+                            <h1>Available Packages List</h1>
+                            <table class="table is-bordered">
+                                <thead>
+                                    <tr><th>Package Name</th><th>Description</th></tr>
+                                </thead>
+                                <tbody>
+                                    ${packageRows}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </nav>
-
-    <div class="container">
-        <h1 id="usage">Usage</h1>
-        <p>To install a specific package using <code>EasyInstall</code>, use the following <code>curl</code> command:</p>
-        <p><code>curl -s https://ezi.sh/[package_name] | sh</code></p>
-        <p>Replace <code>[package_name]</code> with the name of the desired package. For example, to install <code>homebrew</code>, you would use:</p>
-        <p><code>curl -s https://ezi.sh/homebrew | sh</code></p>
-        <h1 id="packages">Available Packages List</h1>
-        <table class="table table-bordered">
-            <thead class="table-dark">
-                <tr><th>Package Name</th><th>Description</th></tr>
-            </thead>
-            <tbody>
-                ${packageRows}
-            </tbody>
-        </table>
-        <p>For more details, visit our <a href="https://github.com/woolflare/EasyInstall">GitHub repository</a>.</p>
+        <div class="hero-foot">
+            <div class="footer has-text-centered">
+                <p>For more details, visit our <a href="https://github.com/woolflare/EasyInstall">GitHub repository</a>.</p>
+            </div>
+        </div>
     </div>
 </body>
 </html>`;
@@ -66,26 +70,46 @@ const generatePackageContent = (key, value) => {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Automatic installation script collection.">
-    <title>EasyInstall - ${key}</title>
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <meta name="description" content="One-Click Script Installation Assembly.">
+    <title>${key} - EasyInstall</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/1.0.1/css/bulma.min.css>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container">
-            <a class="navbar-brand" href="/">EasyInstall</a>
+    <div class="hero is-fullheight">
+        <div class="hero-head">
+            <header>
+                <nav class="navbar has-shadow">
+                    <div class="container">
+                        <div class="navbar-brand">
+                            <a class="navbar-item" href="/">
+                                <span class="title">📦 EasyInstall</span>
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+            </header>
         </div>
-    </nav>
-
-    <div class="container">
-        <h1 id="usage">${key}</h1>
-        <p>${value.description}</p>
-        <p><a href="${value.website}" target="_blank" rel="noopener noreferrer">Website</a> | <a href="${value.installer_source}" target="_blank" rel="noopener noreferrer">Installer Source</a></p>
-        <h1>Install ${key}</h1>
-        <p>Paste this into Terminal.app or a shell prompt, and press enter.</p>
-        <p><code>curl -s https://ezi.sh/${key} | sh</code><p>
-        <p>For more details, visit our <a href="https://github.com/woolflare/EasyInstall">GitHub repository</a>.</p>
+        <div class="hero-body">
+            <div class="container">
+                <div class="columns">
+                    <div class="column is-8 is-offset-2">
+                        <div class="content">
+                            <h1>${key}</h1>
+                            <p>${value.description}</p>
+                            <p><a href="${value.website}" target="_blank" rel="noopener noreferrer">Website</a> | <a href="${value.installer_source}" target="_blank" rel="noopener noreferrer">Installer Source</a></p>
+                            <h1>Install ${key}</h1>
+                            <p>Paste this into Terminal.app or a shell prompt, and press enter.</p>
+                            <pre>curl -s https://ezi.sh/${key} | sh</pre>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="hero-foot">
+            <div class="footer has-text-centered">
+                <p>For more details, visit our <a href="https://github.com/woolflare/EasyInstall">GitHub repository</a>.</p>
+            </div>
+        </div>
     </div>
 </body>
 </html>`;
